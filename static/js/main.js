aa5850c78dcba9ec2560b56427fcd5431366f7b2
Ravil Latypov <ravillatypov12@gmail.com>
2018-11-11 21:14:44 +0300

    начал менять стили и верстку

---
+++ static/js/main.js
@@ -0,0 +1,99 @@
+// var menuExpand = document.querySelector('.show-navbar-menu');
+// var navbarMenu = document.querySelector('.navbar-menu');
+// var menucloser = navbarMenu.querySelector('.menu-close');
+
+// menuExpand.addEventListener('click', function(env){
+//     env.preventDefault();
+//     navbarMenu.classList.add('navbar-menu-active');
+// });
+
+// menucloser.addEventListener('click', function(env){
+//     env.preventDefault();
+//     navbarMenu.classList.remove('navbar-menu-active');
+// });
+
+
+
+
+// var link = document.querySelector(".login-link");
+
+// var popup = document.querySelector(".modal-login");
+// var close = popup.querySelector(".modal-close");
+
+// var form = popup.querySelector("form");
+// var login = popup.querySelector("[name=login]");
+// var password = popup.querySelector("[name=password]");
+
+// var isStorageSupport = true;
+// var storage = "";
+
+// try {
+//     storage = localStorage.getItem("login");
+// } catch (err) {
+//     isStorageSupport = false;
+// }
+
+// link.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     popup.classList.add("modal-show");
+
+//     if (storage) {
+//         login.value = storage;
+//         password.focus();
+//     } else {
+//         login.focus();
+//     }
+// });
+
+// close.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     popup.classList.remove("modal-show");
+//     popup.classList.remove("modal-error");
+// });
+
+// form.addEventListener("submit", function (evt) {
+//     if (!login.value || !password.value) {
+//         evt.preventDefault();
+//         popup.classList.remove("modal-error");
+//         popup.offsetWidth = popup.offsetWidth;
+//         popup.classList.add("modal-error");
+//     } else {
+//         if (isStorageSupport) {
+//             localStorage.setItem("login", login.value);
+//         }
+//     }
+// });
+
+// window.addEventListener("keydown", function (evt) {
+//     if (evt.keyCode === 27) {
+//         evt.preventDefault();
+//         if (popup.classList.contains("modal-show")) {
+//             popup.classList.remove("modal-show");
+//             popup.classList.remove("modal-error");
+//         }
+//     }
+// });
+
+// var mapLink = document.querySelector(".contacts-button-map");
+
+// var mapPopup = document.querySelector(".modal-map");
+// var mapClose = mapPopup.querySelector(".modal-close");
+
+// mapLink.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     mapPopup.classList.add("modal-show");
+// });
+
+// mapClose.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     mapPopup.classList.remove("modal-show");
+// });
+
+// window.addEventListener("keydown", function (evt) {
+//     if (evt.keyCode === 27) {
+//         evt.preventDefault();
+//         if (mapPopup.classList.contains("modal-show")) {
+//             mapPopup.classList.remove("modal-show");
+//         }
+//     }
+// });
