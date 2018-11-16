83af3846a61dc160ca43b4b7a05e39f68652954d
Ravil Latypov <ravillatypov12@gmail.com>
2018-11-16 16:59:30 +0300

    fix UI

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,27 @@ var modalbuttons = document.querySelectorAll('[data-cmd="modalbutton"]');
 var menuclosers = document.querySelectorAll('.close-button');
 var modalclosers = document.querySelectorAll('.modal-close');
 var shadaw = document.querySelector('.full-shadaw');
+var menutitles = document.querySelectorAll('.menu-title');
 
+
+function hideAll() {
+    let modals = document.querySelectorAll('.modal-show');
+    let sidebars = document.querySelectorAll('.sidebar-menu-active');
+    modals.forEach(function (modal, i, node) {
+        modal.classList.remove('modal-show');
+    });
+    sidebars.forEach(function (sidebar, i, node) {
+        sidebar.classList.remove('sidebar-menu-active');
+    });
+    shadaw.classList.remove('full-shadaw-show');
+}
+
+menutitles.forEach(function (menu, i, node) {
+    menu.addEventListener('click', function (evnt) {
+        evnt.preventDefault();
+        menu.parentElement.classList.toggle('menu-opened');
+    });
+});
 sidebarbuttons.forEach(function (btn, i, nodes) {
     btn.addEventListener('click', function (e) {
         e.preventDefault();
@@ -11,7 +31,9 @@ sidebarbuttons.forEach(function (btn, i, nodes) {
             let id = btn.getAttribute('data-id');
             let sbar = document.getElementById(id);
             if (sbar.classList.contains('sidebar-menu')) {
+                hideAll();
                 sbar.classList.add('sidebar-menu-active');
+                sbar.scrollIntoView();
             }
         }
     });
@@ -31,18 +53,6 @@ modalbuttons.forEach(function (btn, i, nodes) {
     });
 });
 
-
-function hideAll() {
-    let modals = document.querySelectorAll('.modal-show');
-    let sidebars = document.querySelectorAll('.sidebar-menu-active');
-    for (modal of modals) {
-        modal.classList.remove('modal-show');
-    }
-    for (sidebar of sidebars) {
-        sidebar.classList.remove('sidebar-menu-active');
-    }
-    shadaw.classList.remove('full-shadaw-show');
-}
 shadaw.addEventListener('click', function (evnt) {
     evnt.preventDefault();
     hideAll();
